4467a6ee8f15bd1acdf42e794e8441f3a4a5f9d8
Mike Kammes <mikekammes@users.noreply.github.com>
2016-01-20 16:00:57 -0500

    changed the navbar again. Now all contests are available to students and people not logged in

--- spec/routing/tournament_spec.rb
+++ spec/routing/tournament_spec.rb
@@ -4,7 +4,7 @@ describe Tournament do
   describe "available routes" do
     specify { expect(get(contest_tournaments_path(1))).to be_routable }
     specify { expect(post(contest_tournaments_path(1))).to be_routable }
-    specify { expect(get(new_contest_tournament_path(1))).to be_routable }
+    specify { expect(get(new_tournament_path(1))).to be_routable }
     specify { expect(get(edit_tournament_path(1))).to be_routable }
     specify { expect(get(tournament_path(1))).to be_routable }
     specify { expect(patch(tournament_path(1))).to be_routable }
